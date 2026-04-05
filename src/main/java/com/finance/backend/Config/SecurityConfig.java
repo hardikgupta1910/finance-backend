77495d5cf8039fbd857c4b41ada83aaf80acb452
JAVA CODE <hardikgupta8109@gmail.com>
2026-04-06 02:37:08 +0530

    Disabled http  basic and form login

--- src/main/java/com/finance/backend/Config/SecurityConfig.java
+++ src/main/java/com/finance/backend/Config/SecurityConfig.java
@@ -20,36 +20,64 @@ public class SecurityConfig {
 	
 
 	
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//		http.csrf(csrf -> csrf.disable())
+//				.sessionManagement(session ->
+//						session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//				)
+//				.authorizeHttpRequests(auth -> auth
+//
+//						//  SWAGGER (allow this)
+//						.requestMatchers(
+//								"/v3/api-docs/**",
+//								"/swagger-ui/**",
+//								"/swagger-ui.html"
+//						).permitAll()
+//
+//						//  AUTH ONLY PUBLIC
+//						.requestMatchers("/auth/**").permitAll()
+//
+//
+//
+//						//  EVERYTHING ELSE SECURED
+//						.anyRequest().authenticated()
+//				)
+//				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+//
+//		return http.build();
+//	}
+//
+	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
-		http.csrf(csrf -> csrf.disable())
+		http
+				.csrf(csrf -> csrf.disable())
+				
 				.sessionManagement(session ->
 						session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				)
+				
+				// 🔥 ADD THESE TWO LINES
+				.formLogin(form -> form.disable())
+				.httpBasic(basic -> basic.disable())
+				
 				.authorizeHttpRequests(auth -> auth
-						
-						//  SWAGGER (allow this)
 						.requestMatchers(
-								"/v3/api-docs/**",
+								"/auth/**",
 								"/swagger-ui/**",
-								"/swagger-ui.html"
+								"/v3/api-docs/**",
+								"/health"
 						).permitAll()
-						
-						//  AUTH ONLY PUBLIC
-						.requestMatchers("/auth/**").permitAll()
-						
-						
-						
-						//  EVERYTHING ELSE SECURED
 						.anyRequest().authenticated()
 				)
+				
 				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 		
 		return http.build();
 	}
-	
-	
 
 	
 }
