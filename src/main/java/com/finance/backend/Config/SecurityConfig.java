16214449fe14ff9c070922a0476358fe6cdd4404
JAVA CODE <hardikgupta8109@gmail.com>
2026-05-02 21:51:23 +0530

    security config

--- src/main/java/com/finance/backend/Config/SecurityConfig.java
+++ src/main/java/com/finance/backend/Config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
 				
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers(
+								"/",
 								"/auth/**",
 								"/swagger-ui/**",
 								"/v3/api-docs/**",
